c2704c93db2ee5a0c61454c09269dafd87a03083
Aleksey Cheusov <vle@gmx.net>
2009-03-27 18:49:02 +0000

    mkc_check_common.sh - common variables and functions for mkc_check_xxx executables

---
+++ mkc_check_common.sh
@@ -0,0 +1,27 @@
+# include file, not executable
+# common variables and functions for mk_check_executables
+
+MKC_CACHEDIR=${MKC_CACHEDIR:=.}
+CC=${CC:=cc}
+
+tmpc=$MKC_CACHEDIR/_mkc_${pathpart}.c
+tmpo=$MKC_CACHEDIR/_mkc_${pathpart}.o
+tmperr=$MKC_CACHEDIR/_mkc_${pathpart}.err
+cache=$MKC_CACHEDIR/_mkc_${pathpart}.res
+
+printme (){
+    if test "$MKC_VERBOSE" != 1; then
+	return
+    fi
+
+    if test "$MKC_SHOW_CACHED" = 1 || test -z "$cached"; then
+	printf "$@"
+    fi
+}
+
+cleanup (){
+    rm -f "$tmpexe" "$tmpo"
+    if test "$MKC_DELETE_TMPFILES" = 1; then
+	rm -f "$tmpc" "$tmperr"
+    fi
+}
