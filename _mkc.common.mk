c7bb3deee18f1af412003f4a2aa03168a7b11a4e
Aleksey Cheusov <vle@gmx.net>
2009-03-27 18:35:54 +0000

    I forget to add it

---
+++ _mkc.common.mk
@@ -0,0 +1,64 @@
+# Copyright (c) 2009, Aleksey Cheusov <vle@gmx.net>
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+.if !defined(NOMKCCOMMON) || empty(NOMKCCOMMON:M[Yy][Ee][Ss])
+
+.if !defined(NOMKCPATHS) || empty(NOMKCPATHS:M[Yy][Ee][Ss])
+# Default PATHs
+PREFIX?=		/usr/local
+BINDIR?=		${PREFIX}/bin
+SBINDIR?=		${PREFIX}/sbin
+MANDIR?=		${PREFIX}/man
+LIBDIR?=		${PREFIX}/lib
+LIBEXECDIR?=		${PREFIX}/libexec
+INCSDIR?=		${PREFIX}/include
+DATADIR?=		${PREFIX}/share
+SYSCONFDIR?=		${PREFIX}/etc
+.endif # nomkcpaths
+
+.if !defined(NOMKCOWN) || empty(NOMKCOWN:M[Yy][Ee][Ss])
+# Default owner and group
+_MKC_UID!=	id -u
+_MKC_GID!=	id -g
+
+MANOWN?=	${_MKC_UID}
+MANGRP?=	${_MKC_GID}
+
+BINOWN?=	${_MKC_UID}
+BINGRP?=	${_MKC_GID}
+
+FILESOWN?=	${_MKC_UID}
+FILESGRP?=	${_MKC_GID}
+
+LIBOWN?=	${_MKC_UID}
+LIBGRP?=	${_MKC_GID}
+
+DOCOWN?=	${_MKC_UID}
+DOCGRP?=	${_MKC_GID}
+
+NLSOWN?=	${_MKC_UID}
+NLSGRP?=	${_MKC_GID}
+.endif # nomkcown
+
+.endif # nomkccommon
