1a759d21a028029cb816aa4b1c918cba0de0a3f3
Aleksey Cheusov <vle@gmx.net>
2009-05-14 19:29:57 +0000

    Makefile.test has been moved to tests/mkc_test.mk

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-env CPPFLAGS="$CPPFLAGS -I$SRCDIR" $MAKE -f $SRCDIR/Makefile.test \
+env CPPFLAGS="$CPPFLAGS -I$SRCDIR" $MAKE -f $SRCDIR/tests/mkc_test.mk \
     > $OBJDIR/_test.res
 
 diff -u $SRCDIR/tests/test.out $OBJDIR/_test.res
