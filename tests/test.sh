18646bd47ce8d853afbddf2429e4f03977d4289d
Aleksey Cheusov <vle@gmx.net>
2009-07-11 15:15:36 +0000

    more regression tests, fixes for them

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-env CPPFLAGS="$CPPFLAGS -I$SRCDIR" $MAKE -f $SRCDIR/tests/mkc_test.mk \
+separator (){
+    printf ' -------------------------------------------\n'
+}
+
+env CPPFLAGS="$CPPFLAGS -I$SRCDIR" $MAKE \
+    -f $SRCDIR/tests/mkc_test.mk \
     > $OBJDIR/_test.res
+separator
+separator >> $OBJDIR/_test.res
+env CPPFLAGS="$CPPFLAGS -I$SRCDIR" $MAKE \
+    -f $SRCDIR/tests/mkc_test_preset.mk \
+    -f $SRCDIR/tests/mkc_test.mk \
+    >> $OBJDIR/_test.res
 
 diff -u $SRCDIR/tests/test.out $OBJDIR/_test.res
