9b9851d7e8f3d7aa6c9933438dca83a37354fc7a
Aleksey Cheusov <vle@gmx.net>
2009-07-11 16:36:04 +0000

    missed file for preset bmake variables

---
+++ tests/mkc_test_preset.mk
@@ -0,0 +1,18 @@
+# I use invalid value 2 here in order to view it in a difference in
+# case of failure
+HAVE_HEADER.sys_time_h=2
+HAVE_HEADER.string_h=2
+HAVE_FUNCLIB.strcpy=2
+HAVE_FUNCLIB.sqrt.m=2
+SIZEOF.int=4
+SIZEOF.long_long=8
+SIZEOF.voidP=8
+SIZEOF.size_t.string_h=4
+HAVE_DEFINE.__BAD_DEFINE__=2
+HAVE_VAR.mkc_test_var.tests_mkc_test_h=2
+HAVE_MEMBER.bad_member=2
+HAVE_MEMBER.bad_member.string_h=2
+HAVE_FUNC2.strcmp.string_h=2
+HAVE_FUNC3.strcpy=2
+HAVE_TYPE.mbstate_t.wchar_h=2
+HAVE_TYPE.bad_type=2
