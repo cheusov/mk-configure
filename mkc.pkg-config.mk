576c42fa1937f4bc291df178e12a235c6ecd68ab
Aleksey Cheusov <vle@gmx.net>
2009-07-18 20:05:20 +0000

    Introduce {LD,CPP}FLAGS.pkg-config.${l} variables

--- mkc.pkg-config.mk
+++ mkc.pkg-config.mk
@@ -1,4 +1,12 @@
 .for l in ${PKG_CONFIG_DEPS}
-CPPFLAGS+=	${:!pkg-config --cflags $l!}
-LDFLAGS+=	${:!pkg-config --libs $l!}
+.if !defined(CPPFLAGS.pkg-config.${l})
+CPPFLAGS.pkg-config.${l}    != pkg-config --cflags ${l}
+.endif
+
+.if !defined(LDFLAGS.pkg-config.${l})
+LDFLAGS.pkg-config.${l}     != pkg-config --libs   ${l}
+.endif
+
+CPPFLAGS+=	${CPPFLAGS.pkg-config.${l}}
+LDFLAGS+=	${LDFLAGS.pkg-config.${l}}
 .endfor
