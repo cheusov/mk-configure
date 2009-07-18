e41a3a8471197f9fb1ebbee8ceb679ef1ed56d9e
Aleksey Cheusov <vle@gmx.net>
2009-07-18 20:11:29 +0000

    clean-ups

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+set -e
+
+test -n "$MAKE"
+test -n "$SRCDIR"
+test -n "$OBJDIR"
+test -n "$MKFILESDIR"
+
+#############################################################
+echo '==================================================' 1>&2
+echo 'Testing mkc.configure.mk...' 1>&2
+
 separator (){
     printf ' -------------------------------------------\n'
 }
@@ -15,3 +26,5 @@ env CPPFLAGS="$CPPFLAGS -I$SRCDIR" $MAKE \
     >> $OBJDIR/_test.res
 
 diff -u $SRCDIR/tests/test.out $OBJDIR/_test.res
+
+echo '' 1>&2
