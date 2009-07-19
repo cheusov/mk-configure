4d3d093b54f916fb8aaff856fc415ca0b30ea3e3
Aleksey Cheusov <vle@gmx.net>
2009-07-19 15:50:33 +0000

    improvements for mkc.minitest.mk and examples

--- mkc.minitest.mk
+++ mkc.minitest.mk
@@ -1,6 +1,9 @@
 .PHONY : test
-test: ${.OBJDIR}/${.CURDIR:T}.test.out
+test:
 	@echo 'Testing ${.CURDIR}... ' 1>&2; \
-	diff -u ${.CURDIR}/expect.out ${.OBJDIR}/${.CURDIR:T}.test.out && \
+	set -e; cd ${.CURDIR}; \
+	tmp_out=${.OBJDIR}/${.CURDIR:T}.test.out; \
+	${MAKE} ${MAKEFLAGS} test_output > $$tmp_out; \
+	diff -u ${.CURDIR}/expect.out $$tmp_out && \
 	echo '      succeeded' 1>&2 || \
 	{ echo '      FAILED' 1>&2; false; }
