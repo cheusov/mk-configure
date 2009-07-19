4a02cb166c0b0576712e851690414dc31489033c
Aleksey Cheusov <vle@gmx.net>
2009-07-19 15:28:53 +0000

    regression tests for "install" and "install-dirs" targets

--- mkc.minitest.mk
+++ mkc.minitest.mk
@@ -1,6 +1,6 @@
 .PHONY : test
-test: ${.CURDIR:T}.test.out
-	@printf 'Testing ${.CURDIR}... ' 1>&2; \
+test: ${.OBJDIR}/${.CURDIR:T}.test.out
+	@echo 'Testing ${.CURDIR}... ' 1>&2; \
 	diff -u ${.CURDIR}/expect.out ${.OBJDIR}/${.CURDIR:T}.test.out && \
-	echo 'succeeded' 1>&2 || \
-	{ echo 'FAILED' 1>&2; false; }
+	echo '      succeeded' 1>&2 || \
+	{ echo '      FAILED' 1>&2; false; }
