4184709c9e8b5f32e14583b5c26c046edd7d1e7a
Aleksey Cheusov <vle@gmx.net>
2009-08-29 17:08:30 +0000

    clean, distclean and uninstall targets are also tested

--- examples/hello_dlopen/test.mk
+++ examples/hello_dlopen/test.mk
@@ -3,12 +3,32 @@ test_output: all
 	@set -e; LC_ALL=C; export LC_ALL; \
 	${.OBJDIR}/dlopen_test | \
 	sed -e 's/0x//' -e 's/[0-9a-fA-F]*$$/F00DBEAF/'; \
-	echo ============================; \
+	\
+	echo =========== all ============; \
+	find ${.OBJDIR} -type f | \
+	test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ========= install ==========; \
 	rm -rf ${.OBJDIR}${PREFIX}; \
 	MKCATPAGES=no; export MKCATPAGES; \
 	${MAKE} ${MAKE_FLAGS} install-dirs install DESTDIR=${.OBJDIR} \
 		> /dev/null; \
 	find ${.OBJDIR}${PREFIX} -type f | \
+	test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ======== uninstall =========; \
+	${MAKE} ${MAKE_FLAGS} uninstall DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR}${PREFIX} -type f | \
+	test_helper "${PREFIX}" "${.OBJDIR}";\
+	\
+	echo ========== clean ===========; \
+	${MAKE} ${MAKE_FLAGS} clean DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR} -type f | \
+	test_helper "${PREFIX}" "${.OBJDIR}";\
+	\
+	echo ======= distclean ==========; \
+	${MAKE} ${MAKE_FLAGS} distclean DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR} -type f | \
 	test_helper "${PREFIX}" "${.OBJDIR}"
 
 .include <mkc.minitest.mk>
