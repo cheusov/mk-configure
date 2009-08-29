28b7d3e8501dab98f82f4008e63c7eea35d569bc
Aleksey Cheusov <vle@gmx.net>
2009-08-29 21:39:30 +0000

    more strange code that should be rechecked and reviewed

--- mkc.pkg-config.mk
+++ mkc.pkg-config.mk
@@ -1,6 +1,21 @@
+.ifndef PKG-CONFIG.exists
+# Solaris has broken which(1). This is why I use pkg-config --help
+PKG-CONFIG.exists!=	pkg-config --help > /dev/null; echo $$?
+.endif
+
+.if ${PKG-CONFIG.exists} != 0
+MKC_ERR_MSG=		pkg-config is not available!
+.else
+
 .for l in ${PKG_CONFIG_DEPS}
+
+PKG-CONFIG.module.exists !=	pkg-config --exists ${l}; echo $$?
+.if ${PKG-CONFIG.module.exists} != 0
+MKC_ERR_MSG=	pkg-config module ${l} is not available
+.else
+
 .if !defined(CPPFLAGS.pkg-config.${l})
-CPPFLAGS.pkg-config.${l}    != pkg-config --cflags ${l}
+CPPFLAGS.pkg-config.${l} !=	pkg-config --cflags ${l}
 .endif
 
 .if !defined(LDADD.pkg-config.${l})
@@ -9,4 +24,9 @@ LDADD.pkg-config.${l}     != pkg-config --libs   ${l}
 
 CPPFLAGS+=	${CPPFLAGS.pkg-config.${l}}
 LDADD+=		${LDADD.pkg-config.${l}}
-.endfor
+
+.endif # .if ${PKG-CONFIG.module.exists}...
+
+.endfor # .for l
+
+.endif # .if ${PKG-CONFIG.exists}...
