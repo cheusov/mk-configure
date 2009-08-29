c1e0dec2a25e8b125b8385f5f6ac6e5e2998067b
Aleksey Cheusov <vle@gmx.net>
2009-08-29 16:18:37 +0000

    mkc_check_sizeof doesn't create file with spaces in its name. examples/hello_sizeof: "make test" also tests "uninstall", "clean" and    "distclean" targets

--- examples/hello_sizeof/test.mk
+++ examples/hello_sizeof/test.mk
@@ -2,12 +2,32 @@
 test_output: all
 	@set -e; \
 	${.OBJDIR}/sizeof_test | tr '1248' 'nnnn'; \
-	echo ============================; \
+	\
+	echo =========== all ============; \
+	find ${.OBJDIR} -type f | \
+	test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ========= install ==========; \
 	rm -rf ${.OBJDIR}${PREFIX}; \
 	MKCATPAGES=no; export MKCATPAGES; \
 	${MAKE} ${MAKE_FLAGS} install-dirs install DESTDIR=${.OBJDIR} \
 		> /dev/null; \
 	find ${.OBJDIR}${PREFIX} -type f | \
+	test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ======== uninstall =========; \
+	${MAKE} ${MAKE_FLAGS} uninstall DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR}${PREFIX} -type f | \
+	test_helper "${PREFIX}" "${.OBJDIR}";\
+	\
+	echo ========== clean ===========; \
+	${MAKE} ${MAKE_FLAGS} clean DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR} -type f | \
+	test_helper "${PREFIX}" "${.OBJDIR}";\
+	\
+	echo ======= distclean ==========; \
+	${MAKE} ${MAKE_FLAGS} distclean DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR} -type f | \
 	test_helper "${PREFIX}" "${.OBJDIR}"
 
 .include <mkc.minitest.mk>
