16a1b5b2d106c598cbbae7f9eb73f308cb9aa196
Aleksey Cheusov <vle@gmx.net>
2009-08-30 15:31:57 +0000

    fixes and improvements for mkc.pkg-config.mk

--- mkc.pkg-config.mk
+++ mkc.pkg-config.mk
@@ -1,17 +1,21 @@
-.ifndef PKG-CONFIG.exists
-# Solaris has broken which(1). This is why I use pkg-config --help
-PKG-CONFIG.exists!=	pkg-config --help > /dev/null; echo $$?
-.endif
+MKC_REQUIRE_PROGS+=	pkg-config
 
-.if ${PKG-CONFIG.exists} != 0
-MKC_ERR_MSG=		pkg-config is not available!
-.else
+.if !defined(PROG.pkg-config)
+MKC_CACHEDIR?=${.OBJDIR}
+MKC_NOCACHE?=
+MKC_DELETE_TMPFILES?=0
+MKC_SHOW_CACHED?=1
+PROG.pkg-config   !=   	env MKC_CACHEDIR='${MKC_CACHEDIR}' MKC_DELETE_TMPFILES='${MKC_DELETE_TMPFILES}' MKC_SHOW_CACHED='${MKC_SHOW_CACHED}' MKC_NOCACHE='${MKC_NOCACHE}' MKC_VERBOSE=1 mkc_check_prog pkg-config
+DISTCLEANFILES+=	${MKC_CACHEDIR}/_mkc_*
+.endif # !defined(PROG.pkg-config)
+
+.if !empty(PROG.pkg-config)
 
 .for l in ${PKG_CONFIG_DEPS}
 
 PKG-CONFIG.module.exists !=	pkg-config --exists ${l}; echo $$?
 .if ${PKG-CONFIG.module.exists} != 0
-MKC_ERR_MSG=	pkg-config module ${l} is not available
+MKC_ERR_MSG=	"ERROR: pkg-config module ${l} cannot be found"
 .else
 
 .if !defined(CPPFLAGS.pkg-config.${l})
@@ -19,14 +23,14 @@ CPPFLAGS.pkg-config.${l} !=	pkg-config --cflags ${l}
 .endif
 
 .if !defined(LDADD.pkg-config.${l})
-LDADD.pkg-config.${l}     != pkg-config --libs   ${l}
+LDADD.pkg-config.${l}    !=	pkg-config --libs   ${l}
 .endif
 
 CPPFLAGS+=	${CPPFLAGS.pkg-config.${l}}
 LDADD+=		${LDADD.pkg-config.${l}}
 
-.endif # .if ${PKG-CONFIG.module.exists}...
+.endif # module exists
 
 .endfor # .for l
 
-.endif # .if ${PKG-CONFIG.exists}...
+.endif # PROG.pkg-config
