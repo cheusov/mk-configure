81e806bae543b56146a8e7a01710ef14c612b540
Aleksey Cheusov <vle@gmx.net>
2009-08-30 20:58:17 +0000

    regression tests: hide strlcpy.o appeared under Linux.     This makes "make test" happy.

--- examples/hello_strlcpy/test.mk
+++ examples/hello_strlcpy/test.mk
@@ -5,28 +5,28 @@ test_output:
 	rm -rf ${.OBJDIR}${PREFIX}; \
 	\
 	echo =========== all ============; \
-	find ${.OBJDIR} -type f | \
+	find ${.OBJDIR} -type f | grep -v 'strlcpy[.]o' | \
 	test_helper "${PREFIX}" "${.OBJDIR}"; \
 	\
 	echo ========= install ==========; \
 	${MAKE} ${MAKE_FLAGS} install-dirs install DESTDIR=${.OBJDIR} \
 		> /dev/null; \
-	find ${.OBJDIR}${PREFIX} -type f | \
+	find ${.OBJDIR}${PREFIX} -type f | grep -v 'strlcpy[.]o' | \
 	test_helper "${PREFIX}" "${.OBJDIR}"; \
 	\
 	echo ======== uninstall =========; \
 	${MAKE} ${MAKE_FLAGS} uninstall DESTDIR=${.OBJDIR} > /dev/null; \
-	find ${.OBJDIR}${PREFIX} -type f | \
+	find ${.OBJDIR}${PREFIX} -type f | grep -v 'strlcpy[.]o' | \
 	test_helper "${PREFIX}" "${.OBJDIR}"; \
 	\
 	echo ========== clean ===========; \
 	${MAKE} ${MAKE_FLAGS} clean DESTDIR=${.OBJDIR} > /dev/null; \
-	find ${.OBJDIR} -type f | \
+	find ${.OBJDIR} -type f | grep -v 'strlcpy[.]o' | \
 	test_helper "${PREFIX}" "${.OBJDIR}"; \
 	\
 	echo ======= distclean ==========; \
 	${MAKE} ${MAKE_FLAGS} distclean DESTDIR=${.OBJDIR} > /dev/null; \
-	find ${.OBJDIR} -type f | \
+	find ${.OBJDIR} -type f | grep -v 'strlcpy[.]o' | \
 	test_helper "${PREFIX}" "${.OBJDIR}"
 
 .include <mkc.minitest.mk>
