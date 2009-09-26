ee5aa7298da0e14460126b42a28b9ad562a3c48a
Aleksey Cheusov <vle@gmx.net>
2009-09-26 20:27:50 +0000

    simplification for mkc_bsd.files.mk; minor code clean-ups

--- mkc_bsd.files.mk
+++ mkc_bsd.files.mk
@@ -1,7 +1,5 @@
 #	$NetBSD: bsd.files.mk,v 1.1.1.1 2006/07/14 23:13:00 jlam Exp $
 
-# This file can be included multiple times.  It clears the definition of
-# FILES at the end so that this is possible.
 .if !defined(_BSD_FILES_MK)
 _BSD_FILES_MK:=	1
 
@@ -9,14 +7,10 @@ _BSD_FILES_MK:=	1
 clean:
 	rm -f ${CLEANFILES}
 
+.if defined(FILES) && !empty(FILES)
+
 .PHONY:		filesinstall
 realinstall:	filesinstall
-
-.if defined(FILES) && !empty(FILES)
-FILESDIR?=${BINDIR}
-FILESOWN?=${BINOWN}
-FILESGRP?=${BINGRP}
-FILESMODE?=${NONBINMODE}
 
 filesinstall:: ${FILES:@F@${DESTDIR}${FILESDIR_${F}:U${FILESDIR}}/${FILESNAME_${F}:U${FILESNAME:U${F:T}}}@}
 .PRECIOUS: ${FILES:@F@${DESTDIR}${FILESDIR_${F}:U${FILESDIR}}/${FILESNAME_${F}:U${FILESNAME:U${F:T}}}@}
@@ -32,10 +26,6 @@ __fileinstall: .USE
 .for F in ${FILES:O:u}
 ${DESTDIR}${FILESDIR_${F}:U${FILESDIR}}/${FILESNAME_${F}:U${FILESNAME:U${F:T}}}: ${F} __fileinstall
 .endfor
-.endif
-
-.if !target(filesinstall)
-filesinstall::
-.endif
+.endif # FILES
 
 .endif # _BSD_FILES_MK
