5603db4f061d8da44a692478ad5759f202a0e205
Aleksey Cheusov <vle@gmx.net>
2009-09-27 09:27:05 +0000

    Support for non-NetBSD platforms where we have no man2html converter   (i.e. have no workable MKHTML=yes). mkc.minitest.mk support expect.${OPSYS}.out

--- mkc.minitest.mk
+++ mkc.minitest.mk
@@ -1,3 +1,7 @@
+.ifndef OPSYS
+OPSYS != uname -s
+.endif
+
 .PHONY : test
 test: all
 	@echo 'Testing ${.CURDIR}... ' 1>&2; \
@@ -6,7 +10,12 @@ test: all
 	rm -f $$tmp_out; \
 	${MAKE} ${MAKEFLAGS} test_output > $$tmp_out.tmp; \
 	mv $$tmp_out.tmp $$tmp_out; \
-	diff -u ${.CURDIR}/expect.out $$tmp_out && \
+	if test -f ${.CURDIR}/expect.${OPSYS}.out; then \
+		expect=${.CURDIR}/expect.${OPSYS}.out; \
+	else \
+		expect=${.CURDIR}/expect.out; \
+	fi; \
+	diff -u $$expect $$tmp_out && \
 	echo '      succeeded' 1>&2 || \
 	{ echo '      FAILED' 1>&2; false; }
 
