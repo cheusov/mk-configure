75de1173a9d2e8056f225eedea18044acde243b3
Aleksey Cheusov <vle@gmx.net>
2009-10-03 21:25:12 +0000

    mkc_check_prog verbose messages contain a path to found program

--- examples/hello_requirements/test.mk
+++ examples/hello_requirements/test.mk
@@ -2,6 +2,7 @@
 test:
 	${MAKE} distclean; \
 	${MAKE} all 2>&1 | \
+	    sed 's|/.*/|/path/to/|' | \
 	    awk '/Error code/ {exit 0} {print}' >${.OBJDIR}/_output.tmp; \
 	diff ${.CURDIR}/expect.out ${.OBJDIR}/_output.tmp 
 
