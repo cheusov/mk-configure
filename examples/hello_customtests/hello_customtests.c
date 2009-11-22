8351766d3970b6e19c7906d2c437b24a48df4efc
Aleksey Cheusov <vle@gmx.net>
2009-11-22 01:48:46 +0000

    Regression tests for C++ and executable MKC_CUSTOM_CHECKS

--- examples/hello_customtests/hello_customtests.c
+++ examples/hello_customtests/hello_customtests.c
@@ -6,12 +6,33 @@
 #include <alloca.h>
 #endif
 
+#ifndef HAVE_CUSTOM_SHTEST
+#define HAVE_CUSTOM_SHTEST 0
+#endif
+
+#ifndef HAVE_CUSTOM_CXX_WITH_TEMPLATES
+#define HAVE_CUSTOM_CXX_WITH_TEMPLATES 0
+#endif
+
+#ifndef HAVE_CUSTOM_TRUE_IS_AVAILABLE
+#define HAVE_CUSTOM_TRUE_IS_AVAILABLE 0
+#endif
+
 int main (int argc, char** argv)
 {
 	if (alloca (100))
 		puts ("alloca(3) succeeded");
 	else
 		puts ("alloca(3) failed");
+
+	printf ("We have C++ compiler with working templates: %s\n",
+			(HAVE_CUSTOM_CXX_WITH_TEMPLATES ? "YES" : "NO"));
+
+	printf ("We have a working 'true' in bourne shell: %s\n",
+			(HAVE_CUSTOM_TRUE_IS_AVAILABLE ? "YES" : "NO"));
+
+	printf ("shtest is good: %s\n",
+			(HAVE_CUSTOM_SHTEST ? "YES" : "NO"));
 
 	return 0;
 }
