5318eae0dc83b69d539f03403af0eea00abbb286
Aleksey Cheusov <vle@gmx.net>
2009-11-22 15:00:37 +0000

    do not fails if not GCC is in use

--- examples/hello_compilers/compiler_test.c
+++ examples/hello_compilers/compiler_test.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
+#ifndef HAVE_DEFINE__MSC_VER
+#define HAVE_DEFINE__MSC_VER 0
+#endif
+
+#ifndef HAVE_DEFINE___INTEL_COMPILER
+#define HAVE_DEFINE___INTEL_COMPILER 0
+#endif
+
+#ifndef HAVE_DEFINE___PCC__
+#define HAVE_DEFINE___PCC__ 0
+#endif
+
+#ifndef HAVE_DEFINE___GNUC__
+#define HAVE_DEFINE___GNUC__ 0
+#endif
+
+char msg [] = "Compiled by " MSG;
+
 int main (int argc, char **argv)
 {
-	printf ("Compiled by %s\n", MSG);
+	printf ("%d\n", (strlen (MSG) > 0) +
+			HAVE_DEFINE__MSC_VER + HAVE_DEFINE___INTEL_COMPILER +
+			HAVE_DEFINE___PCC__ + HAVE_DEFINE___GNUC__);
 
 	return 0;
 }
