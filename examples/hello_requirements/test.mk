7a4effb3f9ae9099fd7d9203a092c5522046e2b9
Aleksey Cheusov <vle@gmx.net>
2009-12-01 16:29:47 +0000

    fix/generalization

--- examples/hello_requirements/test.mk
+++ examples/hello_requirements/test.mk
@@ -2,7 +2,8 @@
 test:
 	${MAKE} distclean; \
 	${MAKE} all 2>&1 | \
-	    sed -e 's|[^ ]* /.*/[^/]*|known ;-) /path/to/program|' \
+	    sed -e 's|for program [^ .]*|for program FOOBAR|' \
+	        -e 's|[.][.][.] /[^ ]*|... /path/to/FOOBAR|' \
 	        -e 's/for compiler type.*$$/for compiler type... known ;-)/' | \
 	    awk '/Error code/ {exit 0} {print}' >${.OBJDIR}/_output.tmp; \
 	diff ${.CURDIR}/expect.out ${.OBJDIR}/_output.tmp 
