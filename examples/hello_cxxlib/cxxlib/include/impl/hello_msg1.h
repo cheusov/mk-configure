696a9cfd6ad230994fa3633b16a36ea0932fae8b
Aleksey Cheusov <vle@gmx.net>
2009-12-23 21:09:12 +0000

    headers

---
+++ examples/hello_cxxlib/cxxlib/include/impl/hello_msg1.h
@@ -0,0 +1,6 @@
+#ifndef _HELLO_MSG1_H_
+#define _HELLO_MSG1_H_
+
+void hello_msg1 ();
+
+#endif // _HELLO_MSG1_H_
---
+++ examples/hello_cxxlib/cxxlib/include/impl/hello_msg2.h
@@ -0,0 +1,6 @@
+#ifndef _HELLO_MSG2_H_
+#define _HELLO_MSG2_H_
+
+std::string hello_msg2 ();
+
+#endif // _HELLO_MSG2_H_
