c9928543f1e88f884efeeb9a319458c362d00d9b
Aleksey Cheusov <vle@gmx.net>
2010-01-30 12:09:03 +0000

    Adapted for tru64/OSF1

--- examples/subprojects/test.mk
+++ examples/subprojects/test.mk
@@ -1,7 +1,5 @@
-.PHONY : test_output basic auto_rpath
-test_output: basic .WAIT auto_rpath
-
-basic:
+.PHONY : test_output
+test_output :
 	@set -e; \
 	rm -rf ${.OBJDIR}${PREFIX}; \
 	LD_LIBRARY_PATH=${.CURDIR}/libhello1:${.CURDIR}/libhello2:$$LD_LIBRARY_PATH; \
@@ -32,18 +30,17 @@ basic:
 	echo ======= distclean ==========; \
 	${MAKE} ${MAKEFLAGS} distclean DESTDIR=${.OBJDIR} > /dev/null; \
 	find ${.OBJDIR} -type f -o -type l | \
-	mkc_test_helper "${PREFIX}" "${.OBJDIR}"
-
-.if ${TARGET_OPSYS} != "NetBSD" && ${TARGET_OPSYS} != "FreeBSD" && ${TARGET_OPSYS} != "DragonFly" && ${TARGET_OPSYS} != "OpenBSD" && ${TARGET_OPSYS} != "MirBSD" && ${TARGET_OPSYS} != "Linux" 
-auto_rpath:
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
 	echo == library dependencies ====; \
 	PREFIX=${.CURDIR}/usr; export PREFIX; \
 	${MAKE} ${MAKEFLAGS} all installdirs install >&2; \
 	${MAKE} ${MAKEFLAGS} cleandir > /dev/null; \
-	${.CURDIR}/usr/bin/hello_subprojects
-.else
-auto_rpath:
-.endif
+	LD_LIBRARY_PATH=${.CURDIR}/usr/lib; \
+	DYLD_LIBRARY_PATH=${.CURDIR}/usr/lib; \
+	export LD_LIBRARY_PATH DYLD_LIBRARY_PATH; \
+	${.CURDIR}/usr/bin/hello_subprojects; \
+	rm -rf ${.CURDIR}/usr
 
 CLEANDIRS+=	${.CURDIR}/usr
 .include <mkc.minitest.mk>
