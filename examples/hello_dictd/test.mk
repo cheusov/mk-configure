bd55797391d7bf1809ea46aee0558f0ed94e5336
Aleksey Cheusov <vle@gmx.net>
2010-05-16 15:17:05 +0000

    regression test for mkc.subprjs.mk

---
+++ examples/hello_dictd/test.mk
@@ -0,0 +1,45 @@
+.PHONY : test_output
+test_output :
+	@set -e; \
+	rm -rf ${.OBJDIR}${PREFIX}; \
+	LD_LIBRARY_PATH=${.CURDIR}/libdz:${.CURDIR}/libmaa:$$LD_LIBRARY_PATH; \
+	DYLD_LIBRARY_PATH=${.CURDIR}/libdz:${.CURDIR}/libmaa:$$LD_LIBRARY_PATH; \
+	export LD_LIBRARY_PATH DYLD_LIBRARY_PATH; \
+	${.CURDIR}/dict/dict; \
+	${.CURDIR}/dictd/dictd; \
+	${.CURDIR}/dictfmt/dictfmt; \
+	${.CURDIR}/dictzip/dictzip; \
+	\
+	echo =========== all ============; \
+	find ${.OBJDIR} -type f -o -type l | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ========= installdirs ==========; \
+	${MAKE} ${MAKEFLAGS} installdirs DESTDIR=${.OBJDIR} \
+		> /dev/null; \
+	find ${.OBJDIR}${PREFIX} -type f -o -type l -o -type d | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ========= install ==========; \
+	${MAKE} ${MAKEFLAGS} install DESTDIR=${.OBJDIR} \
+		> /dev/null; \
+	find ${.OBJDIR}${PREFIX} -type f -o -type l -o -type d | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ======== uninstall =========; \
+	${MAKE} ${MAKEFLAGS} uninstall DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR}${PREFIX} -type f -o -type l | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}";\
+	\
+	echo ========== clean ===========; \
+	${MAKE} ${MAKEFLAGS} clean DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR} -type f -o -type l | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}";\
+	\
+	echo ======= distclean ==========; \
+	${MAKE} ${MAKEFLAGS} distclean DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR} -type f -o -type l | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"
+
+CLEANDIRS+=	${.CURDIR}/usr
+.include <mkc.minitest.mk>
