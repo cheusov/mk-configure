14e04a8e3e6ab3fe6799876c403885b624242a47
Aleksey Cheusov <vle@gmx.net>
2010-06-10 21:08:23 +0000

    New test

---
+++ examples/hello_lua/test.mk
@@ -0,0 +1,34 @@
+DISTCLEANDIRS+=	${.CURDIR}/usr
+
+.PHONY : test_output
+test_output:
+	@set -e; \
+	rm -rf ${.OBJDIR}${PREFIX}; \
+	./foobar; \
+	\
+	echo =========== all ============; \
+	find ${.OBJDIR} -type f | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ========= install ==========; \
+	${MAKE} ${MAKEFLAGS} installdirs install DESTDIR=${.OBJDIR} \
+		> /dev/null; \
+	find ${.OBJDIR}${PREFIX} -type f -o -type d | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ======== uninstall =========; \
+	${MAKE} ${MAKEFLAGS} uninstall DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR}${PREFIX} -type f | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ========== clean ===========; \
+	${MAKE} ${MAKEFLAGS} clean DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR} -type f | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ======= distclean ==========; \
+	${MAKE} ${MAKEFLAGS} distclean DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR} -type f | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"
+
+.include <mkc.minitest.mk>
