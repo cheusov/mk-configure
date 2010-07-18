5093c975abae633393ebcfc2e850f0fc24268424
Aleksey Cheusov <vle@gmx.net>
2010-07-18 21:18:23 +0000

    sys.mk is editable. -1 task to do.

---
+++ sys.mk
@@ -0,0 +1,22 @@
+#
+# mk-configure may not know everything about your platform.
+# This file is for setting for own system-wide default configuration.
+# In addition, @sysconfdir@/etc/mk.conf and /etc/mk.conf
+# may be used by system administrator.
+#
+# Example is below:
+#
+# CC=            /usr/bin/cc
+# CXX=           /usr/bin/CC
+# YACC=          /usr/sfw/bin/bison -y
+# LEX=           /usr/sfw/bin/flex
+# INSTALL=       /usr/ucb/install
+# STRIP=         /usr/ccs/bin/strip
+# LD=            /usr/ccs/bin/ld
+# AR=            /usr/ccs/bin/ar
+# AS=            /usr/ccs/bin/as
+# LORDER=        /usr/ccs/bin/lorder
+# RANLIB=        /usr/ccs/bin/ranlib
+# LUA_LMODDIR=   /usr/share/lua/5.1
+# LUA_CMODDIR=   /usr/lib/lua/5.1
+# ...
