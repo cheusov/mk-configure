fbd274e3dc7bf31ec0f99ba3e7708ad7f60ea73f
Aleksey Cheusov <vle@gmx.net>
2010-07-18 19:50:02 +0000

    Introduce two new variables: LUA_LMODDIR and LUA_CMODDIR,     installation directory for Lua and compiled modules respectively.     Add regression test for them. Add documentation.

--- mkc_imp.lua.mk
+++ mkc_imp.lua.mk
@@ -8,32 +8,41 @@
 
 PKG_CONFIG_DEPS+=	lua
 
+#### .lua modules
 .if defined(LUA_LMODULES)
+.if !defined(LUA_LMODDIR)
 PKG_CONFIG_VARS.lua+=	INSTALL_LMOD
+LUA_LMODDIR?=		${PKG_CONFIG.var.lua.INSTALL_LMOD}
+.endif
 FILES+=			${LUA_LMODULES}
 .for i in ${LUA_LMODULES}
-FILESDIR_${i}=	${PKG_CONFIG.var.lua.INSTALL_LMOD}
+FILESDIR_${i}=	${LUA_LMODDIR}
 .endfor # i
 .endif # defined(LUA_LMODULES)
 
+### .c module
 .if defined(LUA_CMODULE)
+.if !defined(LUA_CMODDIR)
 PKG_CONFIG_VARS.lua+=	INSTALL_CMOD
+LUA_CMODDIR?=		${PKG_CONFIG.var.lua.INSTALL_CMOD}
+.endif
 LIB=			${LUA_CMODULE}
 SRCS?=			${LUA_CMODULE}.c
 MKDLL=			Only
 LDCOMPILER=		Yes
 # The following Line is for Darwin
 #SHLIB_EXT=		.so
-LIBDIR=			${PKG_CONFIG.var.lua.INSTALL_CMOD}
+LIBDIR=			${LUA_CMODDIR}
 .endif # defined(LUA_LMODULES)
 
+######################
 .include <mkc_imp.pkg-config.mk>
 
-.if empty(PKG_CONFIG.var.lua.INSTALL_LMOD)
+.if empty(LUA_LMODDIR)
 MKC_ERR_MSG+=	"ERROR: pkg-config --variable=INSTALL_LMOD lua failed"
 .endif
 
-.if empty(PKG_CONFIG.var.lua.INSTALL_CMOD)
+.if empty(LUA_CMODDIR)
 MKC_ERR_MSG+=	"ERROR: pkg-config --variable=INSTALL_CMOD lua failed"
 .endif
 
