8c92bbcf5e33364f4221e5f6b956d1bc62b77f13
Aleksey Cheusov <vle@gmx.net>
2010-07-24 19:42:18 +0000

    Support for POD-to-HTML conversion.     Regression test (example) for this.     Documentation update.

--- mkc_imp.pod.mk
+++ mkc_imp.pod.mk
@@ -7,18 +7,28 @@
 _MKC_IMP_POD_MK=1
 
 POD2MAN?=		pod2man
-POD2MAN_FLAGS:=		-r '' -n '${.TARGET:T:R}' -c '' ${POD2MAN_FLAGS}
-
+POD2MAN_FLAGS:=		-r '' -n '${.TARGET:T:R}' -c ''
 MESSAGE.pod2man ?=	@${_MESSAGE} "POD2MAN: ${.TARGET}"
 COMPILE.pod2man ?=	${_V} ${POD2MAN} ${POD2MAN_FLAGS}
 
-.SUFFIXES: .pod
+POD2HTML?=		pod2html
+POD2HTML_FLAGS?=
+MESSAGE.pod2html ?=	@${_MESSAGE} "POD2HTML: ${.TARGET}"
+COMPILE.pod2html ?=	${_V} ${POD2HTML} ${POD2HTML_FLAGS}
 
+.SUFFIXES: .pod
 .for i in 1 2 3 4 5 6 7 8 9
 .SUFFIXES: .${i}
 .pod.${i}:
 	${MESSAGE.pod2man}
 	${COMPILE.pod2man} -s ${i} ${.ALLSRC} > ${.TARGET}
 .endfor
+
+.SUFFIXES: .html
+.pod.html:
+	${MESSAGE.pod2html}
+	${COMPILE.pod2html} < ${.ALLSRC} > ${.TARGET}
+
+CLEANFILES+=	pod2htmd.tmp pod2htmi.tmp
 
 .endif # _MKC_IMP_POD_MK
