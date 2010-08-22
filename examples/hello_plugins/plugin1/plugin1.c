1d7c4e50d871febe8e1789ad371dd0450f6ebe01
Aleksey Cheusov <vle@gmx.net>
2010-08-22 13:16:33 +0000

    Initial vbersion of hello_plugins

---
+++ examples/hello_plugins/plugin1/plugin1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void hello_message (void);
+
+void hello_message (void)
+{
+	puts ("Plugin1 sucessfully activated");
+}
---
+++ examples/hello_plugins/plugin2/plugin2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void hello_message (void);
+
+void hello_message (void)
+{
+	puts ("Plugin2 sucessfully activated");
+}
