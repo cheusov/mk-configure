3310a27f2727a03905c2199d37be3b8a7a7fce90
Aleksey Cheusov <vle@gmx.net>
2011-11-29 20:16:58 +0000

    Use ?= instead of = in sys.mk

--- sys.mk
+++ sys.mk
@@ -6,17 +6,17 @@
 #
 # Example is below:
 #
-# CC           = /usr/bin/cc
-# CXX          = /usr/bin/CC
-# YACC         = /usr/sfw/bin/bison -y
-# LEX          = /usr/sfw/bin/flex
-# INSTALL      = /usr/ucb/install
-# STRIP        = /usr/ccs/bin/strip
-# LD           = /usr/ccs/bin/ld
-# AR           = /usr/ccs/bin/ar
-# AS           = /usr/ccs/bin/as
-# LORDER       = /usr/ccs/bin/lorder
-# RANLIB       = /usr/ccs/bin/ranlib
-# LUA_LMODDIR  = /usr/share/lua/5.1
-# LUA_CMODDIR  = /usr/lib/lua/5.1
+# CC           ?= /usr/bin/cc
+# CXX          ?= /usr/bin/CC
+# YACC         ?= /usr/sfw/bin/bison -y
+# LEX          ?= /usr/sfw/bin/flex
+# INSTALL      ?= /usr/ucb/install
+# STRIP        ?= /usr/ccs/bin/strip
+# LD           ?= /usr/ccs/bin/ld
+# AR           ?= /usr/ccs/bin/ar
+# AS           ?= /usr/ccs/bin/as
+# LORDER       ?= /usr/ccs/bin/lorder
+# RANLIB       ?= /usr/ccs/bin/ranlib
+# LUA_LMODDIR  ?= /usr/share/lua/5.1
+# LUA_CMODDIR  ?= /usr/lib/lua/5.1
 # ...
