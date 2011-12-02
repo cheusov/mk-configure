6de03060bac5495734bf4d5da37198412db1361f
Aleksey Cheusov <vle@gmx.net>
2011-12-02 14:50:15 +0000

    Fix for recent changes in Lua support

--- mkc_imp.lua.mk
+++ mkc_imp.lua.mk
@@ -32,7 +32,7 @@ PKG_CONFIG_VARS.lua +=	INSTALL_CMOD
 LUA_CMODDIR         ?=	${PKG_CONFIG.var.lua.INSTALL_CMOD}
 .endif
 LIB        =		${LUA_CMODULE:E}
-SRCS      ?=		${LUA_CMODULE:E}.c
+SRCS      ?=		${LUA_CMODULE:S/./_/g}.c
 MKDLL      =		Only
 LDCOMPILER =		Yes
 DLL_EXT    =		.so
