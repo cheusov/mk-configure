4bd0b36c1e10422b81ad602a54a1735e7577404a
Aleksey Cheusov <vle@gmx.net>
2012-02-18 09:28:07 +0000

    revert recent changes in mkc_imp.lua.mk

--- mkc_imp.lua.mk
+++ mkc_imp.lua.mk
@@ -14,15 +14,10 @@ PKG_CONFIG_DEPS  +=	lua
 PKG_CONFIG_VARS.lua +=	INSTALL_LMOD
 LUA_LMODDIR         ?=	${PKG_CONFIG.var.lua.INSTALL_LMOD}
 .endif
-.for i in ${LUA_LMODULES:R}
-_name_path += ${i:S/./_/g}.lua ${i:S/./\//g}.lua
-.endfor
-.for _name _path in ${_name_path}
-FILES               +=	${_name}
-FILESNAME_${_name}  =	${_path}
-FILESDIR_${_name}   =	${LUA_LMODDIR}
+FILES               +=	${LUA_LMODULES}
+.for i in ${LUA_LMODULES}
+FILESDIR_${i}        =	${LUA_LMODDIR}
 .endfor # i
-.undef _name_path
 .endif # defined(LUA_LMODULES)
 
 ### .c module
@@ -31,15 +26,12 @@ FILESDIR_${_name}   =	${LUA_LMODDIR}
 PKG_CONFIG_VARS.lua +=	INSTALL_CMOD
 LUA_CMODDIR         ?=	${PKG_CONFIG.var.lua.INSTALL_CMOD}
 .endif
-LIB        =		${LUA_CMODULE:C/^.*[.]//}
-SRCS      ?=		${LUA_CMODULE:S/./_/g}.c
+LIB        =		${LUA_CMODULE}
+SRCS      ?=		${LUA_CMODULE}.c
 MKDLL      =		Only
 LDCOMPILER =		Yes
 DLL_EXT    =		.so
-.if !empty(LUA_CMODULE:M*.*)
-LIBDIR     =		${LUA_CMODDIR}/${LUA_CMODULE:R:S/./\//g}
-.endif
-LIBDIR     ?=		${LUA_CMODDIR}
+LIBDIR     =		${LUA_CMODDIR}
 .endif # defined(LUA_LMODULES)
 
 ######################
