158bd03b92cacadc2182c80255725ab62cdff9fa
Aleksey Cheusov <vle@gmx.net>
2012-02-18 12:42:48 +0000

    New regression test for LUA_LMODDIR and LUA_CMODDIR

--- examples/hello_lua/test.mk
+++ examples/hello_lua/test.mk
@@ -23,13 +23,24 @@ test_output:
 	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
 	\
 	echo ========== clean ===========; \
-	${MAKE} ${MAKEFLAGS} clean DESTDIR=${.OBJDIR} > /dev/null; \
+	${MAKE} ${MAKEFLAGS} clean > /dev/null; \
 	find ${.OBJDIR} -type f | \
 	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
 	\
 	echo ======= distclean ==========; \
-	${MAKE} ${MAKEFLAGS} distclean DESTDIR=${.OBJDIR} > /dev/null; \
+	${MAKE} ${MAKEFLAGS} distclean > /dev/null; \
 	find ${.OBJDIR} -type f | \
-	mkc_test_helper "${PREFIX}" "${.OBJDIR}"
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ========= install2 ==========; \
+	env PREFIX=/home/cheusov/local \
+	    LUA_LMODDIR=/home/cheusov/local/share/lua/5.1 \
+	    LUA_CMODDIR=/home/cheusov/local/lib/lua/5.1 \
+	    ${MAKE} ${MAKEFLAGS} all install DESTDIR=${.OBJDIR} \
+		> /dev/null; \
+	find ${.OBJDIR} -type f -o -type d | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}" | uniq; \
+	rm -rf ${.OBJDIR}/home; \
+	${MAKE} ${MAKEFLAGS} distclean > /dev/null
 
 .include <mkc.minitest.mk>
