6b7908353a9d47f6872ff9a56966c527c5a6137a
Aleksey Cheusov <vle@gmx.net>
2012-03-04 19:06:12 +0000

    indentation fix

--- mkc_imp.sys.mk
+++ mkc_imp.sys.mk
@@ -64,13 +64,13 @@ COMPILE.c ?=	${_V} ${CC} ${CFLAGS} ${CPPFLAGS} ${CFLAGS.warnerr} -c
 LINK.c    ?=	${_V} ${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} ${LDFLAGS.prog}
 MESSAGE.c ?=	@${_MESSAGE} "CC: ${.IMPSRC}"
 
-CXX        ?=		c++
+CXX        ?=	c++
 CXXFLAGS   +=	${CFLAGS}
 COMPILE.cc ?=	${_V} ${CXX} ${CXXFLAGS} ${CPPFLAGS} ${CFLAGS.warnerr} -c
 LINK.cc    ?=	${_V} ${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} ${LDFLAGS.prog}
 MESSAGE.cc ?=	@${_MESSAGE} "CXX: ${.IMPSRC}"
 
-OBJC       ?=		${CC}
+OBJC       ?=	${CC}
 OBJCFLAGS  ?=	${CFLAGS}
 COMPILE.m  ?=	${_V} ${OBJC} ${OBJCFLAGS} ${CPPFLAGS} -c
 LINK.m     ?=	${_V} ${OBJC} ${OBJCFLAGS} ${CPPFLAGS} ${LDFLAGS}
