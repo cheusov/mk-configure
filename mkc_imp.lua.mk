65752e6f89b3f742dda8f91112c1173cb3edae0f
Aleksey Cheusov <vle@gmx.net>
2012-08-13 22:51:17 +0300

    Minor fixes in mkc_imp.lua.mk (LUA_MODULES vs. LUA_LMODULES)

--- mkc_imp.lua.mk
+++ mkc_imp.lua.mk
@@ -23,7 +23,7 @@ FILES               +=	${LUA_SRCS.${i}}
 FILESDIR_${LUA_SRCS.${i}} =	${LUA_LMODDIR}/${i:S|.|/|g:H}
 FILESNAME_${LUA_SRCS.${i}} =	${i:S|.|/|g:T}.lua
 .endfor # i
-.endif # defined(LUA_LMODULES)
+.endif # defined(LUA_MODULES)
 
 ### .c module
 .if defined(LUA_CMODULE)
@@ -39,14 +39,14 @@ MKDLL      =		Only
 LDCOMPILER =		Yes
 DLL_EXT    =		.so
 LIBDIR     =		${LUA_CMODDIR}/${LUA_CMODULE:S|.|/|g:H}
-.endif # defined(LUA_LMODULES)
+.endif # defined(LUA_CMODULES)
 
 ######################
 .include <mkc_imp.pkg-config.mk>
 
-.if defined(LUA_LMODULES) && empty(LUA_LMODDIR)
+.if defined(LUA_MODULES) && empty(LUA_LMODDIR)
 MKC_ERR_MSG  +=	"ERROR: pkg-config --variable=INSTALL_LMOD lua failed"
-.endif # LUA_LMODULES
+.endif # LUA_MODULES
 
 .ifdef LUA_CMODULE
 .if empty(LUA_CMODDIR)
@@ -58,4 +58,4 @@ MKC_REQUIRE_HEADERS +=	lua.h
 .endif # !empty(MKC_ERR_MSG)
 .endif # LUA_CMODULE
 
-.endif # LUA_LMODULES) || LUA_CMODULE
+.endif # LUA_MODULES) || LUA_CMODULE
