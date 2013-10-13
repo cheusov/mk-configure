1766f1aa9cad383f30b2f3467081eaf2240ee374
Aleksey Cheusov <vle@gmx.net>
2013-10-13 14:50:15 +0300

    additional files for tests/intexts_cleantrg regression test

---
+++ tests/intexts_cleantrg/test.mk
@@ -0,0 +1,26 @@
+.PHONY : test_output
+test_output :
+	@set -e; \
+	\
+	echo =========== all ============; \
+	find ${.OBJDIR} -type f -o -type l | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ========== clean ===========; \
+	${MAKE} ${MAKEFLAGS} clean DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR} -type f -o -type l | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ======= distclean ==========; \
+	${MAKE} ${MAKEFLAGS} distclean > /dev/null; \
+	find ${.OBJDIR} -type f -o -type l | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ======= cleandir ==========; \
+	${MAKE} ${MAKEFLAGS} distclean > /dev/null; \
+	find ${.OBJDIR} -type f -o -type l | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	${MAKE} ${MAKEFLAGS} cleandir > /dev/null
+
+.include <mkc.minitest.mk>
