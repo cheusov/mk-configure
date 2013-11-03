28854d8b0efda86c37e590a81e67f26642e950c5
Aleksey Cheusov <vle@gmx.net>
2013-11-04 01:25:52 +0300

    New variable BMAKE_REQD

--- mkc_imp.preinit.mk
+++ mkc_imp.preinit.mk
@@ -3,6 +3,20 @@
 # See COPYRIGHT file in the distribution.
 ############################################################
 
+####################
+BMAKE_REQD ?=	20110606
+
+.ifdef MAKE_VERSION
+_bmake_ok != test ${MAKE_VERSION} -ge ${BMAKE_REQD} && echo 1 || echo 0
+.else
+_bmake_ok  = 0
+.endif
+
+.if !${_bmake_ok}
+.error "bmake-${BMAKE_REQD} or newer is required"
+.endif
+
+####################
 .if !make(clean) && !make(cleandir) && !make(distclean)
 MKCHECKS ?=	yes
 .else
--- tests/reqd4/test.mk
+++ tests/reqd4/test.mk
@@ -4,6 +4,7 @@ test:
 	tmp_out=${.OBJDIR}/${.CURDIR:T}.test.out; \
 	{ \
 	  ${MAKE} ${MAKEFLAGS} -j3 all 2>&1 | sed -n 2p; \
+	  ${MAKE} ${MAKEFLAGS} -j3 all MAKE_VERSION=00000000 2>&1 | sed 's/^.*"bmake/"bmake/'; \
 	  echo =========== all ============; \
 	  find ${.OBJDIR} -type f -o -type l | \
 	  mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
