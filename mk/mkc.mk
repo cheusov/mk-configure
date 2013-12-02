d5ee37d2cfb0366270ce74c097b3d6533bd74625
Aleksey Cheusov <vle@gmx.net>
2013-12-03 00:51:36 +0300

    add mk/mkc.mk

---
+++ mk/mkc.mk
@@ -0,0 +1,20 @@
+# Copyright (c) 2013 by Aleksey Cheusov
+#
+# See LICENSE file in the distribution.
+############################################################
+
+.ifndef _MKC_MK
+_MKC_MK := 1
+
+init_make_level ?= 0
+.if defined(TOPDIR) && ${TOPDIR:U} != ${.CURDIR} && ${.MAKE.LEVEL} == ${init_make_level}
+MKC_CACHEDIR ?=	${TOPDIR}
+.export MKC_CACHEDIR
+.MAIN: all
+.DEFAULT:
+	@set -x; set -e; cd ${TOPDIR}; pwd; ${MAKE} ${MAKEFLAGS} ${.TARGET}-${.CURDIR:T}; set +x
+.else
+.include <mkc_imp.mk>
+.endif #TOPDIR
+
+.endif # _MKC_MK
