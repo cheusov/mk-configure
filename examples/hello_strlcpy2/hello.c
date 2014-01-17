39d2fdc79f38269cbef8174d9edec9aca6d53774
Aleksey Cheusov <vle@gmx.net>
2014-01-17 19:29:48 +0300

    Add regression test for MKC_SOURCE_DIR

---
+++ examples/hello_strlcpy2/hello.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+#ifndef HAVE_FUNC3_STRLCPY_STRING_H
+size_t strlcpy(char *dst, const char *src, size_t siz);
+#endif
+
+#ifndef HAVE_FUNC3_GETLINE_STDIO_H
+ssize_t getline(char** lineptr, size_t* n, FILE* stream);
+#endif
+
+int main (int argc, char ** argv)
+{
+	char *buf = NULL;
+	size_t size = 0;
+	ssize_t len = 0;
+	char small_buf [10];
+
+	while (len = getline (&buf, &size, stdin), len != -1){
+		len = strlen (buf);
+		if (len > 0 && buf [len-1] == '\n')
+			buf [len-1] = 0;
+
+		strlcpy (small_buf, buf, sizeof (small_buf));
+		puts (small_buf);
+	}
+
+	return 0;
+}
---
+++ examples/hello_strlcpy2/test.mk
@@ -0,0 +1,38 @@
+.PHONY : test_output
+test_output:
+	@set -e; \
+	${.OBJDIR}/hello < ${.CURDIR}/input.in; \
+	rm -rf ${.OBJDIR}${PREFIX}; \
+	MKCATPAGES=yes; export MKCATPAGES; \
+	\
+	echo =========== all ============; \
+	find ${.OBJDIR} -type f | grep -Ev '(strlcpy|getline)[.]o' | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ========= install ==========; \
+	${MAKE} ${MAKEFLAGS} install -j3 DESTDIR=${.OBJDIR} \
+		> /dev/null; \
+	find ${.OBJDIR}${PREFIX} -type f -o -type d | \
+	grep -vE '(strlcpy|getline)[.]o' | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ======== uninstall =========; \
+	${MAKE} ${MAKEFLAGS} -j4 uninstall DESTDIR=${.OBJDIR} > /dev/null; \
+	find ${.OBJDIR}${PREFIX} -type f | grep -vE '(strlcpy|getline)[.]o' | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ========== clean ===========; \
+	${MAKE} ${MAKEFLAGS} clean > /dev/null; \
+	find ${.OBJDIR} -type f | grep -vE '(strlcpy|getline)[.]o' | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ======= distclean ==========; \
+	${MAKE} ${MAKEFLAGS} distclean > /dev/null; \
+	find ${.OBJDIR} -type f | grep -vE '(strlcpy|getline)[.]o' | \
+	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	\
+	echo ======= CLEANFILES ==========; \
+	${MAKE} ${MAKEFLAGS} print-values VARS='CLEANFILES' MKCHECKS=no | \
+	awk '{for(i=1; i<=NF; ++i) if ($$i ~ /[.]o$$/) print $$i}'
+
+.include <mkc.minitest.mk>
