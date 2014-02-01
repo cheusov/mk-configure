c12bbe4d66082b1ac6a881688886d0776c367576
Aleksey Cheusov <vle@gmx.net>
2014-02-01 16:10:22 +0300

    Make examples/hello_strlcpy3 funnier

--- examples/hello_strlcpy3/hello.c
+++ examples/hello_strlcpy3/hello.c
@@ -5,12 +5,15 @@
 #include <mkc_strlcat.h>
 #include <mkc_getline.h>
 
+static const char message [] = "Theo de Raadt said: \"The strlcpy() and strlcat() functions provide a consistent, unambiguous API to help the programmer write more bullet-proof code.\"";
+
 int main (int argc, char ** argv)
 {
 	char *buf = NULL;
 	size_t size = 0;
 	ssize_t len = 0;
 	char small_buf [15];
+	char said [19];
 
 	while (len = getline (&buf, &size, stdin), len != -1){
 		len = strlen (buf);
@@ -21,6 +24,9 @@ int main (int argc, char ** argv)
 		strlcat (small_buf, buf, sizeof (small_buf));
 		puts (small_buf);
 	}
+
+	strlcpy (said, message, sizeof (said));
+	puts (said);
 
 	return 0;
 }
