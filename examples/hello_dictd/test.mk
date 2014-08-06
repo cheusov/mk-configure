ac50a32af07e210a3d20b317d139880f27b34e92
Aleksey Cheusov <vle@gmx.net>
2014-08-06 21:29:41 +0300

    Yet another fix in regression tests

--- examples/hello_dictd/test.mk
+++ examples/hello_dictd/test.mk
@@ -64,7 +64,7 @@ test_output :
 	${MAKE} ${MAKEFLAGS} distclean > /dev/null; \
 	find ${.OBJDIR} -type f -o -type l | \
 	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
-	rm -rf ${.OBJDIR}${PREFIX} ${.OBJDIR}/usr ${.OBJDIR}/home; \
+	rm -rf ${.OBJDIR}${PREFIX} ${.OBJDIR}/usr ${.OBJDIR}/home ${.OBJDIR}/Users; \
 	echo =========== MKOBJDIRS=auto ============; \
 	env TARGETS=fake ${MAKE} ${MAKEFLAGS} fake \
 		MKCHECKS=no MAKEOBJDIRPREFIX=${.OBJDIR}/obj1 > /dev/null; \
