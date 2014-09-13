ef39c1598fe3ca860e9be277251ccc6fe20836b0
Aleksey Cheusov <vle@gmx.net>
2014-09-14 01:33:49 +0300

    Fix examples/hello_compatlib test on non-BSD systems

--- examples/hello_compatlib/test.mk
+++ examples/hello_compatlib/test.mk
@@ -1,8 +1,10 @@
+FUNCS_RE=(fgetln|progname|strlcat|strlcpy|getline)[.]o
+
 .PHONY : test_output
 test_output :
 	@set -e; \
 	echo =========== all ============; \
-	find ${.OBJDIR} -type f | \
+	find ${.OBJDIR} -type f | grep -Ev '${FUNCS_RE}' | \
 	grep -vE 'libcmpt/.*[.]os?$$' | \
 	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
 	\
