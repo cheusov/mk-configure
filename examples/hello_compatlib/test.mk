bbd6e4ac32fb543159a5a326c06b1f310c196a19
Aleksey Cheusov <vle@gmx.net>
2014-11-26 00:53:40 +0300

    examples/hello_compatlib: more complex test for COMPATLIBS (fix recent)

--- examples/hello_compatlib/test.mk
+++ examples/hello_compatlib/test.mk
@@ -1,12 +1,17 @@
-FUNCS_RE=(fgetln|progname|strlcat|strlcpy|getline)[.]o
+FUNCS_RE=(fgetln|progname|strlcat|strlcpy|getline|_mkcfake)[.]o
 
 .PHONY : test_output
 test_output :
 	@set -e; \
 	echo =========== all ============; \
-	find ${.OBJDIR} -type f | grep -Ev '${FUNCS_RE}' | \
-	grep -vE 'libcmpt/.*[.]os?$$' | \
-	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	{ find ${.OBJDIR} -type f | \
+	  LC_ALL=C sort | \
+	  grep -Ev '${FUNCS_RE}|prog1/|prog2/'; \
+	  echo ===; \
+	  find ${.OBJDIR}/prog1 ${.OBJDIR}/prog2 -type f | \
+	  LC_ALL=C sort; \
+	} | \
+	env NOSORT=1 mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
 	\
 	true =========== cleandir ============; \
 	${MAKE} ${MAKEFLAGS} distclean > /dev/null
