8e0f94a13a8f34712e942cfec84f7be5e5d71398
Aleksey Cheusov <vle@gmx.net>
2014-11-26 17:20:18 +0000

    Fixes for "err" and "warn" features. Now verr, verrx, vwarn and vwarnx functions are detected correctly with gcc-4.9

--- features/mkc_imp.f_err.mk
+++ features/mkc_imp.f_err.mk
@@ -11,14 +11,22 @@ _MKC_IMP_F_ERR_MK := 1
 
 MKC_CHECK_HEADERS       +=	err.h
 MKC_CHECK_FUNCLIBS      +=	err errx verr verrx
-MKC_CHECK_FUNCS3        +=	err:err.h errx:err.h verr:err.h verrx:err.h
+MKC_CHECK_FUNCS3        +=	err:err.h errx:err.h
+
+MKC_CHECK_PROTOTYPES       +=	verr
+MKC_PROTOTYPE_FUNC.verr     =	void verr(int, const char *, va_list)
+MKC_PROTOTYPE_HEADERS.verr  =	stdarg.h err.h
+
+MKC_CHECK_PROTOTYPES       +=	verrx
+MKC_PROTOTYPE_FUNC.verrx    =	void verrx(int, const char *, va_list)
+MKC_PROTOTYPE_HEADERS.verrx =	${MKC_PROTOTYPE_HEADERS.verr}
 
 .include <mkc_imp.conf-cleanup.mk>
 
 .if ${HAVE_FUNCLIB.err:U0} && ${HAVE_FUNCLIB.errx:U0} && \
     ${HAVE_FUNCLIB.verr:U0} && ${HAVE_FUNCLIB.verrx:U0} && \
     ${HAVE_FUNC3.err.err_h:U0} && ${HAVE_FUNC3.errx.err_h:U0} && \
-    ${HAVE_FUNC3.verr.err_h:U0} && ${HAVE_FUNC3.verrx.err_h:U0}
+    ${HAVE_PROTOTYPE.verr:U0} && ${HAVE_PROTOTYPE.verrx:U0}
 CFLAGS +=	-DMKC_ERR_IS_FINE
 .else
 MKC_SRCS +=		${FEATURESDIR}/err/err.c
--- features/mkc_imp.f_warn.mk
+++ features/mkc_imp.f_warn.mk
@@ -11,14 +11,22 @@ _MKC_IMP_F_WARN_MK := 1
 
 MKC_CHECK_HEADERS       +=	err.h
 MKC_CHECK_FUNCLIBS      +=	warn warnx vwarn vwarnx
-MKC_CHECK_FUNCS2        +=	warn:err.h warnx:err.h vwarn:err.h vwarnx:err.h
+MKC_CHECK_FUNCS2        +=	warn:err.h warnx:err.h
+
+MKC_CHECK_PROTOTYPES       +=	vwarn
+MKC_PROTOTYPE_FUNC.vwarn     =	void vwarn(const char *, va_list)
+MKC_PROTOTYPE_HEADERS.vwarn  =	stdarg.h err.h
+
+MKC_CHECK_PROTOTYPES       +=	vwarnx
+MKC_PROTOTYPE_FUNC.vwarnx    =	void vwarnx(const char *, va_list)
+MKC_PROTOTYPE_HEADERS.vwarnx =	${MKC_PROTOTYPE_HEADERS.vwarn}
 
 .include <mkc_imp.conf-cleanup.mk>
 
 .if ${HAVE_FUNCLIB.warn:U0} && ${HAVE_FUNCLIB.warnx:U0} && \
     ${HAVE_FUNCLIB.vwarn:U0} && ${HAVE_FUNCLIB.vwarnx:U0} && \
     ${HAVE_FUNC2.warn.err_h:U0} && ${HAVE_FUNC2.warnx.err_h:U0} && \
-    ${HAVE_FUNC2.vwarn.err_h:U0} && ${HAVE_FUNC2.vwarnx.err_h:U0}
+    ${HAVE_PROTOTYPE.vwarn:U0} && ${HAVE_PROTOTYPE.vwarnx:U0}
 CFLAGS +=	-DMKC_WARN_IS_FINE
 .else
 MKC_SRCS +=	${FEATURESDIR}/warn/warn.c
