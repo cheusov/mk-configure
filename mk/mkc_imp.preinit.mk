c1bb3ce042c945bcf0ec750282fc363329fabf48
Aleksey Cheusov <vle@gmx.net>
2015-11-12 00:00:46 +0300

    Magic :Q when necessary

--- mk/mkc_imp.preinit.mk
+++ mk/mkc_imp.preinit.mk
@@ -10,7 +10,7 @@
 BMAKE_REQD ?=	20110606
 
 .ifdef MAKE_VERSION
-_bmake_ok != test ${MAKE_VERSION} -ge ${BMAKE_REQD} && echo 1 || echo 0
+_bmake_ok != test ${MAKE_VERSION:Q} -ge ${BMAKE_REQD:Q} && echo 1 || echo 0
 .else
 _bmake_ok  = 0
 .endif
