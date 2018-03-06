c7684b419d22f2e48cf7328cbe99571bf9cfba1f
Aleksey Cheusov <vle@gmx.net>
2018-03-06 13:55:12 +0000

    tests/pkg_config_2: fix test

--- tests/pkg_config_2/test.mk
+++ tests/pkg_config_2/test.mk
@@ -4,7 +4,7 @@ test:
 	tmp_out=${.OBJDIR}/${.CURDIR:T}.test.out; \
 	rm -f $$tmp_out; \
 	${MAKE} ${MAKEFLAGS} all 2>&1 | \
-	awk '/ERROR:/ && !/has version/ {print $$1}' > $$tmp_out || true; \
+	awk '/ERROR:/ && !/has version/ {print $$1}' | uniq > $$tmp_out || true; \
 	if diff ${.CURDIR}/expect.out $$tmp_out; \
 	then echo '      succeeded' 1>&2; \
 	else echo '      FAILED' 1>&2; ex=1; \
