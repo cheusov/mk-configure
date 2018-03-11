2791e68e098be97760fdeeb9aade7310cea4e92a
Aleksey Cheusov <vle@gmx.net>
2018-03-11 10:18:52 +0200

    hello_lua3 test: yet another "standard" directory for lua /opt/pkgsrc4unix

--- examples/hello_lua3/test.mk
+++ examples/hello_lua3/test.mk
@@ -12,12 +12,12 @@ test_output:
 	echo ========= install ==========; \
 	${MAKE} ${MAKEFLAGS} install -j3 DESTDIR=${.OBJDIR} \
 		> /dev/null; \
-	find ${.OBJDIR}/usr -type f | \
+	find ${.OBJDIR}/usr ${.OBJDIR}/opt -type f | \
 	mkc_test_helper "${PREFIX}" "${.OBJDIR}" | uniq; \
 	\
 	echo ======== uninstall =========; \
 	${MAKE} ${MAKEFLAGS} -j4 uninstall DESTDIR=${.OBJDIR} > /dev/null; \
-	find ${.OBJDIR}/usr -type f | \
+	find ${.OBJDIR}/usr ${.OBJDIR}/opt -type f | \
 	mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
 	\
 	echo ========== clean ===========; \
