d5bffcfbf4950eea79207b6425fe430f65f969c2
Aleksey Cheusov <vle@gmx.net>
2019-12-12 17:22:15 +0300

    Add unit test for recent MKC_CHECK_SIZEOF fix

--- tests/os_NetBSD/test.mk
+++ tests/os_NetBSD/test.mk
@@ -1,8 +1,9 @@
 .PHONY : test_output
 test_output:
 .for v in ${VARIABLES_TO_CHECK}
-	@echo ${v}=${${v}}
+	@echo ${v}=${${v}} | sed 's/[1-9]/n/g'
 .endfor
+	@echo CFLAGS=${CFLAGS} | sed 's/[1-9]/n/g'
 	@${MAKE} ${MAKEFLAGS} cleandir > /dev/null 2>&1
 
 .include <mkc.minitest.mk>
