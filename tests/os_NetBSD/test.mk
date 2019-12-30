bf5752b5412b57a88a07bbca4ef786ad30d44ebf
Aleksey Cheusov <vle@gmx.net>
2019-12-30 13:22:37 +0300

    Fix tests/os_NetBSD/test.mk

--- tests/os_NetBSD/test.mk
+++ tests/os_NetBSD/test.mk
@@ -1,9 +1,9 @@
 .PHONY : test_output
 test_output:
 .for v in ${VARIABLES_TO_CHECK}
-	@echo ${v}=${${v}} | sed 's/[1-9]/n/g'
+	@echo ${v}=${${v}} | sed 's/=[1-9]/=n/g'
 .endfor
-	@echo CFLAGS=${CFLAGS} | sed 's/[1-9]/n/g'
+	@echo CFLAGS=${CFLAGS} | sed 's/=[1-9]/=n/g'
 	@${MAKE} ${MAKEFLAGS} cleandir > /dev/null 2>&1
 
 .include <mkc.minitest.mk>
