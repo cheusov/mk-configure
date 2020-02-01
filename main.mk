d27c26ae302d3b25c35f772ded33a01e580bbe8a
Aleksey Cheusov <vle@gmx.net>
2020-02-01 23:27:10 +0300

    Rename examples/help project to examples/help_target

--- main.mk
+++ main.mk
@@ -29,7 +29,7 @@ examples    =	hello_world hello_scripts hello_files hello_sizeof hello_lex \
    hello_lua3 hello_superfs hello_xxzip hello_progs hello_progs2 tools \
    tools2 pkgconfig3 hello_SLIST hello_RBTREE hello_errwarn hello_fgetln \
    hello_autotools hello_autoconf hello_libdeps hello_compatlib \
-   hello_require_tools check_compiler_opts help # fts (fts.h)
+   hello_require_tools check_compiler_opts help_target # fts (fts.h)
 .for t in ${examples}
 SUBPRJ +=	examples/${t}:tests
 .endfor
