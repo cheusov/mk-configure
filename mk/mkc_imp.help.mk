fbf098f2c3c5483da13e74330068e03328aabee4
Aleksey Cheusov <vle@gmx.net>
2020-02-04 09:57:15 +0300

    Fix mkc_imp.help.mk

--- mk/mkc_imp.help.mk
+++ mk/mkc_imp.help.mk
@@ -15,16 +15,16 @@ help:
 	@echo "There are the following variables you may use to configure ${PROJECTNAME}:"
 .   for v in ${USE_VARIABLES}
 .       if !empty(${v}.descr)
-	    @echo "  * ${v} (${${v}.descr})"
+	    @echo "  * "${v:Q}" ("${${v}.descr}")"
 .	else
-	    @echo "  * ${v}"
+	    @echo "  * "${v:Q}
 .	endif
 .       if !empty(${v}.0)
-	    @echo "    - ${${v}.0} (the default)"
+	    @echo "    - "${${v}.0}" (the default)"
 .       endif
 .       for n in 1 2 3 4 5 6 7 8 9 10
 .           if !empty(${v}.${n})
-		@echo "    - ${${v}.${n}}"
+		@echo "    - "${${v}.${n}}
 .           endif
 .       endfor # n
 .   endfor # USE_VARIABLES
