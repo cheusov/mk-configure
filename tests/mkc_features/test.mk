81a01ae7a388faf505cff55cbd1e5dca8debf303
Aleksey Cheusov <vle@gmx.net>
2020-02-07 00:25:00 +0300

    Improve tests/mkc_features; Now it checks that all feature objects are inside COMPATLIB only

---
+++ tests/mkc_features/test.mk
@@ -0,0 +1,10 @@
+.PHONY : test_output
+test_output :
+	@set -e; \
+	echo =========== all ============; \
+	find ${.OBJDIR} -type f | grep '/tool/.*[.]o$$' | sort | \
+	    mkc_test_helper ${PREFIX:Q} ${.OBJDIR:Q}; \
+	: =========== cleandir ============; \
+	${MAKE} ${MAKEFLAGS} cleandir > /dev/null
+
+.include <mkc.minitest.mk>
