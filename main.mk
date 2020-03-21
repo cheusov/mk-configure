3e3dcad6631e45af9256298eb9d10334a763b3bc
Aleksey Cheusov <vle@gmx.net>
2020-03-21 18:11:07 +0300

    List of tests/ and examples/ are set in newly created file tests.mk

--- main.mk
+++ main.mk
@@ -57,27 +57,11 @@ SUBPRJ_DFLT =   builtins examples/helpers mk scripts features doc
 SUBPRJ      =	scripts:examples scripts:presentation ${SUBPRJ_DFLT} \
 		scripts:builtins scripts:examples/helpers scripts:mk scripts:features scripts:doc
 
-tests       =	configure_test mkinstall mkshlib mkstaticlib mkpiclib \
-   mkprofilelib mkdll pkg_config_0 pkg_config_1 pkg_config_1_1 pkg_config_2 \
-   lua_dirs rec_makefiles reqd reqd2 reqd3 reqd4 reqd_clean_cache \
-   intexts_cleantrg require_prototype test_subprj_dash test_mkc_vs_PROG \
-   test_mkc_vs_LIB test_mkc_vs_SUBDIR test_mkc_vs_SUBPRJ endianness \
-   create_cachedir sys_queue predopost_targets FSRCDIR mkc_features \
-   os_NetBSD os_OpenBSD os_Linux dltest mkc_install mkc_check_custom \
-   cccxx_type
+.include "tests.mk"
 .for t in ${tests}
 SUBPRJ +=	tests/${t}:tests
 .endfor
 
-examples    =	hello_world hello_scripts hello_files hello_sizeof hello_lex \
-   hello_yacc hello_calc2 subprojects hello_compilers hello_plugins \
-   hello_plugins2 hello_glib2 hello_subdirs hello_strlcpy hello_strlcpy2 \
-   hello_strlcpy3 hello_customtests hello_customtests2 hello_requirements \
-   hello_iconv hello_cxx hello_cxxlib hello_dictd hello_lua hello_lua2 \
-   hello_lua3 hello_superfs hello_xxzip hello_progs hello_progs2 tools \
-   tools2 pkgconfig3 hello_SLIST hello_RBTREE hello_errwarn hello_fgetln \
-   hello_autotools hello_autoconf hello_libdeps hello_compatlib \
-   hello_require_tools check_compiler_opts help_target # fts (fts.h)
 .for t in ${examples}
 SUBPRJ +=	examples/${t}:examples
 .endfor
