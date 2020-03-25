b11e53fcb8ace01d1ee3903082e61d867c5921ef
Aleksey Cheusov <vle@gmx.net>
2020-03-25 18:59:44 +0300

    tests/dltest: run "cleandir" at the end of "test"

--- tests/dltest/test.mk
+++ tests/dltest/test.mk
@@ -1,6 +1,8 @@
 .PHONY : test_output
 test_output:
 	@set -e; \
-	for l in ${LDADD}; do echo $$l; done | awk '/-ldl/ {++cnt} END {print (cnt <= 1)}'
+	for l in ${LDADD}; do echo $$l; done | awk '/-ldl/ {++cnt} END {print (cnt <= 1)}'; \
+	: =========== cleandir ============; \
+	${MAKE} ${MAKEFLAGS} cleandir > /dev/null
 
 .include <mkc.minitest.mk>
