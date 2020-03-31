2f47c2b199fc7b8248a95fe339bb6959ef45a385
Aleksey Cheusov <vle@gmx.net>
2020-03-31 22:19:06 +0300

    tests/os_NetBSD/test.mk: fix for CFLAGS containing -D_FORTIFY_SOURCE=n

--- tests/os_NetBSD/test.mk
+++ tests/os_NetBSD/test.mk
@@ -3,7 +3,7 @@ test_output:
 .for v in ${VARIABLES_TO_CHECK}
 	@echo ${v}=${${v}} | sed 's/=[1-9]/=n/g'
 .endfor
-	@echo CFLAGS=${CFLAGS:M-D*} | sed 's/=[1-9]/=n/g'
+	@echo CFLAGS=${CFLAGS:M-D*:N-D_FORTIFY_SOURCE=*} | sed 's/=[1-9]/=n/g'
 	@${MAKE} ${MAKEFLAGS} cleandir > /dev/null 2>&1
 
 .include <mkc.minitest.mk>
