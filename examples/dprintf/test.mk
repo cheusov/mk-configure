4f159c9f39592194dc02708d3977a220592b8b15
Aleksey Cheusov <vle@gmx.net>
2020-11-17 15:59:35 +0000

    Newly created tests run "cleandir" target at the end of "test"

--- examples/dprintf/test.mk
+++ examples/dprintf/test.mk
@@ -1,5 +1,6 @@
 .PHONY : test_output
 test_output:
-	@${.OBJDIR}/prog 2>&1 >/dev/null 
+	@${.OBJDIR}/prog 2>&1 >/dev/null; \
+	${MAKE} ${MAKEFLAGS} cleandir > /dev/null
 
 .include <mkc.minitest.mk>
--- examples/vis/test.mk
+++ examples/vis/test.mk
@@ -1,5 +1,6 @@
 .PHONY : test_output
 test_output:
-	@${.OBJDIR}/prog
+	@${.OBJDIR}/prog; \
+	${MAKE} ${MAKEFLAGS} cleandir > /dev/null
 
 .include <mkc.minitest.mk>
