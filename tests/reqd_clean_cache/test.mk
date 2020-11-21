373fa4776046472444d16f41ff1101a6413f79b2
Aleksey Cheusov <vle@gmx.net>
2020-11-21 19:22:08 +0000

    Adaptations for FreeBSD make

--- tests/reqd_clean_cache/test.mk
+++ tests/reqd_clean_cache/test.mk
@@ -7,7 +7,7 @@ test_output :
 	echo =========== all ============; \
 	{ ${MAKE} ${MAKEFLAGS} all 2>/dev/null || true; \
 	find ${.OBJDIR} -type f -o -type l | sort; } | \
-	sed 's,n*bmake\[[0-9]*\],bmake,' | \
+	sed 's,n*b*make\[[0-9]*\],bmake,' | \
 	env NOSORT=1 mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
 	\
 	${MAKE} ${MAKEFLAGS} cleandir > /dev/null
