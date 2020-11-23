1f0423c6614dfce50a6b81a37efd6133b0eb37b8
Aleksey Cheusov <vle@gmx.net>
2020-11-23 18:57:48 +0000

    Simplify tests/os_{NetBSD,OpenBSD,Linux} tests

--- tests/os_NetBSD/test.mk
+++ tests/os_NetBSD/test.mk
@@ -1,9 +1,9 @@
 .PHONY : test_output
 test_output:
 .for v in ${VARIABLES_TO_CHECK}
-	@echo ${v}=${${v}} | sed 's/=[1-9]/=n/g'
+	@echo ${v}=${${v}}
 .endfor
-	@echo CPPFLAGS=${CPPFLAGS:M-D*:N-D_FORTIFY_SOURCE=*} | sed 's/=[1-9]/=n/g'
+	@echo CPPFLAGS=${CPPFLAGS:M-D*:N-D_FORTIFY_SOURCE=*}
 	@${MAKE} ${MAKEFLAGS} cleandir > /dev/null 2>&1
 
 .include <mkc.minitest.mk>
