92e442d1973a0102941af8b9cdf1b61043e12816
Aleksey Cheusov <vle@gmx.net>
2020-11-23 20:05:59 +0100

    Update tests/os_Linux

--- tests/os_NetBSD/test.mk
+++ tests/os_NetBSD/test.mk
@@ -1,9 +1,9 @@
 .PHONY : test_output
 test_output:
 .for v in ${VARIABLES_TO_CHECK}
-	@echo ${v}=${${v}}
+	@echo ${v}=${${v}} | sed 's/=[48]/=n/g'
 .endfor
-	@echo CPPFLAGS=${CPPFLAGS:M-D*:N-D_FORTIFY_SOURCE=*}
+	@echo CPPFLAGS=${CPPFLAGS:M-D*:N-D_FORTIFY_SOURCE=*} | sed 's/=[48]/=n/g'
 	@${MAKE} ${MAKEFLAGS} cleandir > /dev/null 2>&1
 
 .include <mkc.minitest.mk>
