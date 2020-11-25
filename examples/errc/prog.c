39bc9ac16dca987e96d259aa0b3991a3e16c4c74
Aleksey Cheusov <vle@gmx.net>
2020-11-25 14:22:16 +0300

    examples/errc: fix for systems with neither getprogname(3) nor program_invocation_short_name variable

--- examples/errc/prog.c
+++ examples/errc/prog.c
@@ -2,11 +2,14 @@
 #include <errno.h>
 
 #include <mkc_errc.h>
+#include <mkc_progname.h>
 
 static const char bad_filename[] = "/bad/file.txt";
 
 int main (int argc, char **argv)
 {
+	setprogname("prog");
+
 	FILE *fd = fopen(bad_filename, "r");
 	if (!fd)
 		errc(15, errno, "Cannot open file %s", bad_filename);
