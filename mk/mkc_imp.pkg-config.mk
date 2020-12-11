cheusov/mk-configure

commit 5d2f6ad5fe24206e8b7ee8d142f70adad106494c

Internal mk files are included only if it necessary.   For example, mkc_imp.incs.mk is included only if   variable INCS is not empty. In theory, this should   speed-up mkcmake.

mk/mkc_imp.pkg-config.mk
@@ -3,12 +3,11 @@
 # See LICENSE file in the distribution.
 ############################################################
 
-.if defined(MKC_REQUIRE_PKGCONFIG)
-MKC_CHECK_PKGCONFIG +=		${MKC_REQUIRE_PKGCONFIG}
-.endif
+.if ${MKCHECKS} == "yes"
 
-.if defined(MKC_CHECK_PKGCONFIG) && ${MKCHECKS} == "yes"
-MKC_REQUIRE_PROGS+=	pkg-config
+MKC_CHECK_PKGCONFIG +=	${MKC_REQUIRE_PKGCONFIG:U}
+MKC_REQUIRE_PROGS   +=	pkg-config
+
 .include <mkc.conf.mk>
 
 .if ${HAVE_PROG.pkg-config}

doc/TODO
@@ -1,7 +1,4 @@
 ============================================================
-Speed-up processing by .ifdef-ing unnecessary .include-s.
-
-============================================================
 SHRTOUT -- set -x and redirect everything to temporary
 log file and print it only if the target fails?
 

examples/files/expect.out
@@ -1,5 +1,5 @@
 =========== vars ============
-CLEANFILES=/path/to/files.test.out pod2htmd.tmp pod2htmi.tmp  shell_func.cat7 shell_func.html7 shell_func.info  
+CLEANFILES=/path/to/files.test.out pod2htmd.tmp pod2htmi.tmp  shell_func.cat7 shell_func.html7 shell_func.info
 CLEANDIRFILES= /path/to/_mkc_*
 =========== all ============
 /objdir/DEBIAN/control

mk/mkc_imp.files.mk
@@ -7,16 +7,9 @@
 # See LICENSE file in the distribution.
 ############################################################
 
-.if !defined(_BSD_FILES_MK)
-_BSD_FILES_MK := 1
-
 filesinstall: .PHONY # ensure existence
 
-.include <mkc.init.mk>
-
 do_install1:	.PHONY filesinstall
-
-.if defined(FILES) && !empty(FILES)
 
 realdo_all: ${FILES}
 
@@ -41,6 +34,3 @@ ${DESTDIR}${FILESDIR_${F}:U${FILESDIR}}/${FILESNAME_${F}:U${FILESNAME:U${F:T}}}:
 UNINSTALLFILES  +=	${destination_files}
 INSTALLDIRS     +=	${destination_files:H}
 .endif # MKINSTALL
-.endif # FILES
-
-.endif # _BSD_FILES_MK

mk/mkc_imp.inc.mk
@@ -1,4 +1,4 @@
-# Copyright (c) 2009-2010 by Aleksey Cheusov
+# Copyright (c) 2009-2020 by Aleksey Cheusov
 # Copyright (c) 1994-2009 The NetBSD Foundation, Inc.
 # Copyright (c) 1988, 1989, 1993 The Regents of the University of California
 # Copyright (c) 1988, 1989 by Adam de Boor

mk/mkc_imp.info.mk
@@ -7,12 +7,7 @@
 # See LICENSE file in the distribution.
 ############################################################
 
-.if !defined(_MKC_IMP_INFO_MK) && defined(TEXINFO)
-_MKC_IMP_INFO_MK := 1
-
 infoinstall: .PHONY
-
-.include <mkc.init.mk>
 
 MAKEINFO     ?=	makeinfo
 INFOFLAGS    ?=	
@@ -26,7 +21,6 @@ MESSAGE.texinfo ?=	@${_MESSAGE} "TEXINFO: ${.TARGET}"
 	${MESSAGE.texinfo}
 	${_V}${MAKEINFO} ${INFOFLAGS} --no-split -o $@ $<
 
-.if defined(TEXINFO) && !empty(TEXINFO)
 realdo_all: ${TEXINFO}
 
 INFOFILES =	${TEXINFO:S/.texinfo/.info/g:S/.texi/.info/g:S/.txi/.info/g}
@@ -62,7 +56,3 @@ UNINSTALLFILES  +=	${destination_infos}
 INSTALLDIRS     +=	${destination_infos:H}
 .endif # MKINSTALL
 .endif # MKINFO
-
-.endif # TEXINFO
-
-.endif # _MKC_IMP_INFO_MK

mk/mkc_imp.intexts.mk
@@ -8,9 +8,6 @@
 # @sysconfdir@, @libdir@, @bindir@, @sbindir@, @datadir@ etc. with
 # real ${PREFIX}, ${SYSCONFDIR} etc. See examples/ projects.
 
-.ifndef _MKC_IMP_INTEXTS_MK
-_MKC_IMP_INTEXTS_MK := 1
-
 MESSAGE.gen ?=	@${_MESSAGE} "GEN: ${.TARGET}"
 
 INTEXTS_SED  +=	-e 's,@sysconfdir@,${SYSCONFDIR},g'
@@ -54,6 +51,3 @@ ${i:T} : ${i}.in
 CLEANFILES   +=	${INSCRIPTS:T} ${INFILES:T}
 
 realdo_all: ${INSCRIPTS:T} ${INFILES:T}
-
-######################################################################
-.endif # _MKC_IMP_INTEXTS_MK

mk/mkc_imp.links.mk
@@ -3,9 +3,6 @@
 # See LICENSE file in the distribution.
 ############################################################
 
-.if !defined(_MKC_IMP_LINKS_MK)
-_MKC_IMP_LINKS_MK := 1
-
 linksinstall:	.PHONY # ensure existence
 
 do_install2:	linksinstall
@@ -31,4 +28,3 @@ INSTALLDIRS    +=	${DESTDIR}${r:H}
 .endfor
 
 .endif # MKINSTALL=yes
-.endif # _MKC_IMP_LINKS_MK

commit c0f696ed3bdb40854a72095499b5474b990a51d4

Fix bug in mk/mkc_imp.pkg-config.mk introduced by 5d2f6ad5fe24206e8b7ee8d142f70adad106494c

mk/mkc_imp.pkg-config.mk
@@ -3,7 +3,11 @@
 # See LICENSE file in the distribution.
 ############################################################
 
-.if ${MKCHECKS} == "yes"
+.if !empty(MKC_REQUIRE_PKGCONFIG)
+MKC_CHECK_PKGCONFIG +=		${MKC_REQUIRE_PKGCONFIG}
+.endif
+
+.if ${MKCHECKS} == "yes" && !empty(MKC_CHECK_PKGCONFIG)
 
 MKC_CHECK_PKGCONFIG +=	${MKC_REQUIRE_PKGCONFIG:U}
 MKC_REQUIRE_PROGS   +=	pkg-config