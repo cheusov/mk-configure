7b9a2a68cacd28391dd27a0206b871adf6c94710
Aleksey Cheusov <vle@gmx.net>
2021-03-30 01:41:08 +0300

    Add new feature "realloarr" for function from NetBSD-8 and later

--- examples.mk
+++ examples.mk
@@ -9,4 +9,4 @@ examples    =	hello_world files files2 sizeof lexer \
    require_tools check_compiler_opts help_target \
    arc4random bswap dprintf efun strsep errc posix_getopt \
    raise_default_signal reallocarray fparseln vis \
-   fts humanize_number shquote tiny_id c99
+   fts humanize_number shquote tiny_id c99 reallocarr
--- tests/mkc_features/tool/test_features1.cxx
+++ tests/mkc_features/tool/test_features1.cxx
@@ -24,6 +24,7 @@
 #include "mkc_strsep.h"
 #include "mkc_posix_getopt.h"
 #include "mkc_raise_default_signal.h"
+#include "mkc_reallocarr.h"
 #include "mkc_reallocarray.h"
 #include "mkc_fparseln.h"
 #include "mkc_vis.h"
@@ -65,6 +66,7 @@ int main(int argc, char** argv)
 	free(ptr);
 	getopt(0, NULL, NULL);
 	raise_default_signal(15);
+	reallocarr(NULL, 0, 0);
 	reallocarray(NULL, 0, 0);
 	fparseln(NULL, NULL, NULL, "\\\\#", 0);
 	vis(NULL, 0, 0, 0);
