b681c5152ede6f33b040ebe8531518361153ea32
Aleksey Cheusov <vle@gmx.net>
2021-04-02 22:06:46 +0300

    feature "macro": adoptions for NetBSD where __constfunc, __pure etc. exist

--- features/mkc_macro.h
+++ features/mkc_macro.h
@@ -19,37 +19,45 @@
 #  endif
 #endif
 
-#ifdef HAVE_NO_ATTR_CONST
-# define __constfunc
-#else
-# define __constfunc __attribute__((const))
+#ifndef __constfunc
+#  ifdef HAVE_NO_ATTR_CONST
+#    define __constfunc
+#  else
+#    define __constfunc __attribute__((const))
+#  endif
 #endif
 
-#ifdef HAVE_NO_ATTR_NORETURN
-# define __dead
-#else
-# define __dead __attribute__((noreturn))
+#ifndef __dead
+#  ifdef HAVE_NO_ATTR_NORETURN
+#    define __dead
+#  else
+#    define __dead __attribute__((noreturn))
+#  endif
 #endif
 
-#ifdef HAVE_NO_ATTR_PURE
-# define __pure
-#else
-# define __pure __attribute__((pure))
+#ifndef __pure
+#  ifdef HAVE_NO_ATTR_PURE
+#    define __pure
+#  else
+#    define __pure __attribute__((pure))
+#  endif
 #endif
 
-#ifdef HAVE_NO_ATTR_PRINTFLIKE
-# define __printflike
-#else
-# define __printflike(fmtarg, firstvararg) \
+#ifndef __printflike
+#  ifdef HAVE_NO_ATTR_PRINTFLIKE
+#    define __printflike
+#  else
+#    define __printflike(fmtarg, firstvararg) \
 	__attribute__((format (printf, fmtarg, firstvararg)))
+#  endif
 #endif
 
 #ifndef _DIAGASSERT
-#define _DIAGASSERT(c) assert(c)
+#  define _DIAGASSERT(c) assert(c)
 #endif
 
 #ifndef __UNCONST
-#define __UNCONST(p) ((void *) ((char *)0 + ((const char *)(p) - (const char *)0)))
+#  define __UNCONST(p) ((void *) ((char *)0 + ((const char *)(p) - (const char *)0)))
 #endif
 
 #endif /* _MKC_MACRO_H_ */
