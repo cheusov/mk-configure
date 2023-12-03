5c5d4d46eaf60b2bca0e0c7a1298d85478bf1dcd
Aleksey Cheusov <vle@gmx.net>
2023-12-03 21:57:59 +0400

    Fix test tests/predopost_targets; it may fail on systems without lua installed

--- tests/predopost_targets/test.mk
+++ tests/predopost_targets/test.mk
@@ -3,13 +3,13 @@ test_output :
 	@set -e; \
 	\
 	{ \
-	echo '=========== {pre,do,post}_recursive ============'; \
-	for t in errorcheck clean cleandir install all uninstall installdirs depend; do \
-		${MAKE} ${MAKEFLAGS} -j1 $$t | \
-		grep -Ev 'checking|rm -rf '; \
-	done ; \
-	env MKINSTALLDIRS=no ${MAKE} ${MAKEFLAGS} -j1 install; \
-	} | env NOSORT=1 mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
+	   echo '=========== {pre,do,post}_recursive ============'; \
+	   for t in errorcheck clean cleandir install all uninstall installdirs depend; do \
+		${MAKE} ${MAKEFLAGS} -j1 $$t 2>&1; \
+	   done; \
+	   env MKINSTALLDIRS=no ${MAKE} ${MAKEFLAGS} -j1 install; \
+	} | grep -Ev 'checking|rm -rf ' | \
+	env NOSORT=1 mkc_test_helper "${PREFIX}" "${.OBJDIR}"; \
 	\
 	echo '=========== {pre,do,post}_nonrec ============'; \
 	for t in bin_tar bin_targz bin_tarbz2 bin_zip bin_deb; do \
