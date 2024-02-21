67fba503284c9bb249128ebe3aa2cd09cb983646
Aleksey Cheusov <vle@gmx.net>
2024-02-22 01:10:05 +0400

    Improvements in examples/multilibs/test.mk

--- examples/multilibs/test.mk
+++ examples/multilibs/test.mk
@@ -3,11 +3,11 @@ FUNCS_RE='multilibs.test.out.tmp'
 .PHONY : test_output
 test_output:
 	@set -e; \
-	LD_LIBRARY_PATH=${.CURDIR}/libhello1:${.CURDIR}/libhello2:$$LD_LIBRARY_PATH; \
-	DYLD_LIBRARY_PATH=${.CURDIR}/libhello1:${.CURDIR}/libhello2:$$LD_LIBRARY_PATH; \
+	LD_LIBRARY_PATH=${OBJDIR_libsummator}:${OBJDIR_libmultiplier}:$$LD_LIBRARY_PATH; \
+	DYLD_LIBRARY_PATH=${LD_LIBRARY_PATH}; \
 	export LD_LIBRARY_PATH DYLD_LIBRARY_PATH; \
 	while read a b; do \
-		${.OBJDIR}/app $$a $$b; \
+		${OBJDIR_app}/app $$a $$b; \
 	done < ${.CURDIR}/input.in; \
 	\
 	echo ========== depend ==========; \
