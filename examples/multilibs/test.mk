2e7a3693fd31038cb72f9c9311a63d6f5851373f
Aleksey Cheusov <vle@gmx.net>
2024-03-01 10:41:42 -0600

    Fix examples/multilibs/test.mk on Darwin

--- examples/multilibs/test.mk
+++ examples/multilibs/test.mk
@@ -4,7 +4,7 @@ FUNCS_RE='multilibs.test.out.tmp'
 test_output:
 	@set -e; \
 	LD_LIBRARY_PATH=${OBJDIR_libsummator}:${OBJDIR_libmultiplier}:$$LD_LIBRARY_PATH; \
-	DYLD_LIBRARY_PATH=${LD_LIBRARY_PATH}; \
+	DYLD_LIBRARY_PATH=$$LD_LIBRARY_PATH; \
 	export LD_LIBRARY_PATH DYLD_LIBRARY_PATH; \
 	while read a b; do \
 		${OBJDIR_app}/app $$a $$b; \
