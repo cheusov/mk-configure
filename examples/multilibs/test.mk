e450c55487c439100600a258a48dcba1969b774e
Aleksey Cheusov <vle@gmx.net>
2024-03-02 18:14:12 +0400

    Improve examples/multilibs/test.mk

--- examples/multilibs/test.mk
+++ examples/multilibs/test.mk
@@ -8,6 +8,12 @@ test_output:
 	export LD_LIBRARY_PATH DYLD_LIBRARY_PATH; \
 	while read a b; do \
 		${OBJDIR_app}/app $$a $$b; \
-	done < ${.CURDIR}/input.in
+	done < ${.CURDIR}/input.in; \
+	\
+	echo '=========== all ============'; \
+	find ${.CURDIR} -type f | grep -Ev '${FUNCS_RE}' | \
+	mkc_test_helper ${PREFIX:Q} ${.OBJDIR:Q} ${.CURDIR:Q}; \
+	\
+	${MAKE} -C${.CURDIR} ${MAKEFLAGS} cleandir > /dev/null
 
 .include <mkc.minitest.mk>
